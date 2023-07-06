dcf9420affbd3c717589e5cb67a58351c65c34e1
Timothy <tyl1@williams.edu>
2023-07-06 14:55:56 -0400

    new

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '324182131042-8n7je6088b1q80frgu3vv2vssf6vnjjb.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    githubClientID: '88e04ff6caff21ef36e0',
+    githubClientSecret: 'b5e3b883df04b0127328ad556a60ae56e8ed66cf',
+    mongoURI: 'mongodb+srv://Kim:INdiYRThEVQhISG8@cluster0.p9gwppx.mongodb.net/emailydb?retryWrites=true&w=majority',
+    cookieKey: 'iaodfasdfjakldhflkjahldkjfhakjdlhjkabcjkbajksd' // random string of chars that no one is guessing
+};
